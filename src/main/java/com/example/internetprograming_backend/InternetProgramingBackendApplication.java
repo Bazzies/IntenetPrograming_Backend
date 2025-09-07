79e4b82df680ca32b7d4b0c076ce7bdc58118ee8
ByoungJae <bottlebaek5231@gmail.com>
2025-09-07 14:41:53 +0900

    feat : init setting

---
+++ src/main/java/com/example/internetprograming_backend/InternetProgramingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.internetprograming_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetProgramingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetProgramingBackendApplication.class, args);
+    }
+
+}
